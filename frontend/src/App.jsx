862366a0279378de992c5f5124c3a952372e22ea
ishan-surana <ishan5.mitmpl2022@learner.manipal.edu>
2025-04-10 13:43:38 +0530

    Signup logic and Bootstrapified alerts added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,13 +11,13 @@ import RequireAuth from "./RequireAuth";
 function App() {
   return (
     <Router>
-<Routes>
-  <Route path="/" element={<LoginPage />} />
-  <Route path="/home" element={<RequireAuth><LandingPage /></RequireAuth>} />
-  <Route path="/game" element={<RequireAuth><Game /></RequireAuth>} />
-  <Route path="/results" element={<RequireAuth><Results /></RequireAuth>} />
-  <Route path="/play" element={<RequireAuth><ChessBoard /></RequireAuth>} />
-</Routes>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/home" element={<RequireAuth><LandingPage /></RequireAuth>} />
+        <Route path="/game" element={<RequireAuth><Game /></RequireAuth>} />
+        <Route path="/results" element={<RequireAuth><Results /></RequireAuth>} />
+        <Route path="/play" element={<RequireAuth><ChessBoard /></RequireAuth>} />
+      </Routes>
     </Router>
   );
 }
